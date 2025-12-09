6b72f056ce901f721641a9dbf084a523b6545f09
JGP1-CSI <jamiepick4@gmail.com>
2025-12-09 17:39:08 +0000

    The start of getting better

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import {
+    Card
+} from '../components/ui/card.tsx';
+
+
+const App = () => {
+    return (
+        <Card>
+            Hello, Wikiformers!
+        </Card>
+    )
+}
+
+
+export default App;
